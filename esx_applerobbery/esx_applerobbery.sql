73df37a514252218ff009f54bf6fd2ba0a459ad3
Losjonis1 <100728628+Losjonis1@users.noreply.github.com>
2022-04-07 16:14:05 +0200

    Add files via upload

---
+++ esx_applerobbery/esx_applerobbery.sql
@@ -0,0 +1,11 @@
+-- Para los que tengan "weight"
+
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('iphone', 'iPhone', 1, 0, 1)
+;
+
+-- Para los que tengan "limit"
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('iphone', 'iPhone', 1, 0, 1)
+;
